79bd09cc8f5dae7062268bdc77e50e1f6ea0c3e0
Hien Luong <hyhien@gmail.com>
2022-10-19 07:16:55 +0700

    Update ReadMe

--- MyApiNetCore6/Program.cs
+++ MyApiNetCore6/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using MyApiNetCore6.Data;
 using MyApiNetCore6.Repositories;
@@ -57,7 +56,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
